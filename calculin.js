83d701dc3cf662daa8c5e3347515015ee4523a8f
eljonims <eljonims@gmail.com>
2017-09-24 00:21:30 +0200

    primer comportamiento javascript

---
+++ calculin.js
@@ -0,0 +1,30 @@
+
+var respuesta = [];
+var factores = [];
+var solucion;
+
+
+function nuevoProducto(){
+	factores[0] = Math.floor(10 * Math.random());
+	factores[1] = Math.floor(10 * Math.random());
+	solucion = factores[0]*factores[1];
+	document.getElementById("pregunta").innerHTML=factores.join(" * ");
+	document.getElementById("respuesta").innerHTML="";	
+}
+
+window.addEventListener("load",nuevoProducto);
+
+
+function tecla( cual )
+{
+	if (cual < 0 && respuesta.length > 0){
+		respuesta.pop();
+		document.getElementById("respuesta").innerHTML=respuesta.join("");
+	}else{
+		respuesta.push(cual);
+		document.getElementById("respuesta").innerHTML=respuesta.join("");
+		if( solucion == Number.parseInt(respuesta.join(""))){
+			setTimeOut(nuevoProducto(),0);
+		}		
+	}		
+}
